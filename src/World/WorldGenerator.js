ed6af35980ed1ebd212741e703c8a5f3fb65ccea
Aenoris <aenoriss@gmail.com>
2022-04-09 20:31:37 -0300

    sada

--- src/World/WorldGenerator.js
+++ src/World/WorldGenerator.js
@@ -17,7 +17,7 @@ function WorldGenerator(props) {
 
   useEffect(() => {
     // WebGPU()
-    let positions = generateHexen(3);
+    let positions = generateHexen(2);
     let biomes = placeBiomes(positions);
     // console.log("PORTAL", props)
     setWorld(biomes);
