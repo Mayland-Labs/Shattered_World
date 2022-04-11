d1afa847b68592a5b30e486e17766761c12ddc7f
Aenoris <aenoriss@gmail.com>
2022-04-11 07:24:40 -0300

    hex

--- src/World/WorldGenerator.js
+++ src/World/WorldGenerator.js
@@ -17,7 +17,7 @@ function WorldGenerator(props) {
 
   useEffect(() => {
     // WebGPU()
-    let positions = generateHexen(1);
+    let positions = generateHexen(4);
     let biomes = placeBiomes(positions);
     console.log("items", props.items.staff)
     // console.log("PORTAL", props)
